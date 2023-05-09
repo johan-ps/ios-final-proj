6a131b38520a864da47b60fb9f3d014072b57d7c
Emily Kinder <89366821+EmKinder@users.noreply.github.com>
2023-05-09 14:57:12 +1000

    init - project file created

---
+++ ios_TicketingApp/ios_TicketingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios_TicketingApp
+//
+//  Created by Clair Kinder on 9/5/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
