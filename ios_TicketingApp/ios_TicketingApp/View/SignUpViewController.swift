c2c7c64f40a94f0ba27ad8f56bb493bb313eeeb0
Emily Kinder <89366821+EmKinder@users.noreply.github.com>
2023-05-30 16:03:19 +1000

    signupviewcontroller fixed

--- ios_TicketingApp/ios_TicketingApp/View/SignUpViewController.swift
+++ ios_TicketingApp/ios_TicketingApp/View/SignUpViewController.swift
@@ -27,6 +27,16 @@ class SignUpViewController: UIViewController {
         for user in users{
             if user.email == emailTextField.text {
                 flag = false
+
+                    let alert = UIAlertController(title: "Email already in use", message: "Enter a new email or login", preferredStyle: .alert)
+
+                    alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "This closes alert"), style: .default, handler: { _ in
+                    NSLog("The \"OK\" alert occured.")
+                    }))
+
+                    self.present(alert, animated: true, completion: nil)
+                    return
+                
             }
         }
         if let firstName = firstNameTextField?.text,
@@ -38,12 +48,33 @@ class SignUpViewController: UIViewController {
            !email.isEmpty,
            !password.isEmpty,
            flag{
-            
-            let newUser = User(firstName: firstName, lastName: lastName, email: email, password: password)
-            let fileWriter = FileWriter()
-            fileWriter.addUser(user: newUser)
-            
-            performSegue(withIdentifier: "goToConcertsSignUp", sender: newUser)
+            if(!isValidEmail(email)){
+                let alert = UIAlertController(title: "Invalid Input", message: "Email format not valid", preferredStyle: .alert)
+                
+                alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "This closes alert"), style: .default, handler: { _ in
+                    NSLog("The \"OK\" alert occured.")
+                }))
+                
+                self.present(alert, animated: true, completion: nil)
+                return
+            }
+            else{
+                let newUser = User(firstName: firstName, lastName: lastName, email: email, password: password)
+                let fileWriter = FileWriter()
+                fileWriter.addUser(user: newUser)
+                
+                performSegue(withIdentifier: "goToConcertsSignUp", sender: newUser)
+            }
+        }
+        else{
+            let alert = UIAlertController(title: "Invalid Input", message: "All fields must be completed", preferredStyle: .alert)
+
+            alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "This closes alert"), style: .default, handler: { _ in
+            NSLog("The \"OK\" alert occured.")
+            }))
+
+            self.present(alert, animated: true, completion: nil)
+            return
         }
         firstNameTextField.text = ""
         lastNameTextField.text = ""
@@ -57,5 +88,12 @@ class SignUpViewController: UIViewController {
                 concertVC.user = sender as? User
             }
         }
+    }
+    
+    func isValidEmail(_ email: String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+
+        let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailPred.evaluate(with: email)
     }
 }
